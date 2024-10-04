f324a3051d01f8e495717c91f4e4efbed69836c1
Wanleyrooby <alexroob96@gmail.com>
2024-10-04 14:35:43 -0300

    User entity and resource

---
+++ src/main/java/com/wanroo/project/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.wanroo.project.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wanroo.project.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "888888", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}

56eac6766d7ae6d460dc7c0a3db93c18db8851c9
Wanleyrooby <alexroob96@gmail.com>
2024-10-07 11:49:16 -0300

    Category

---
+++ src/main/java/com/wanroo/project/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.wanroo.project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wanroo.project.entities.Category;
+import com.wanroo.project.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
